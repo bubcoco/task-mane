da96b2f0efdd3931f15d747a94900917b6687bf1
COCO <bulletflip22@gmail.com>
2025-09-17 03:35:51 +0700

    feature: service

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+    // tasksService: TasksService;
+
+    constructor(private tasksService: TasksService) {
+        // this.tasksService = tasksService;
+    }
+
+    
+}
