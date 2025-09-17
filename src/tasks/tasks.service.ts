46dbdae17c7f042b87620ed34d1582ebb540ea94
COCO <bulletflip22@gmail.com>
2025-09-17 17:59:56 +0700

    feature: createTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task } from './task.model';
+import { TaskStatus } from './task.model';
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: Math.random().toString(36).substring(2, 15),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
     }
 }
